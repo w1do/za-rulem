a27f35ee6bd8b2c480ada84f34260970458ba59c
W1DO DIGITAL <uniqdeveloper@yandex.ru>
2026-07-14 22:42:00 +0500

    fix: site zone

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://za-rulem.ru',
+  site: 'https://za-rulem.org',
   trailingSlash: 'never',
   integrations: [
     react(),
