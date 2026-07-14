33cfb1ca257ebce6071696e4e392ab268e6cdd5d
W1DO DIGITAL <uniqdeveloper@yandex.ru>
2026-07-14 20:44:05 +0500

    add: metrics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://za-rulem.ru',
-	integrations: [react()],
+  site: 'https://za-rulem.ru',
+  integrations: [react()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
